8abf5ec156cee31cee60742bf26b76f4776fa354
MothraShiny <paperairplaneinhd@gmail.com>
2023-08-19 16:27:23 +0300

    Remove useless annotations

--- src/main/java/com/example/spingRestDemo/mappers/PersonMapper.java
+++ src/main/java/com/example/spingRestDemo/mappers/PersonMapper.java
@@ -3,14 +3,12 @@ package com.example.spingRestDemo.mappers;
 import com.example.spingRestDemo.DTO.PersonDTO;
 import com.example.spingRestDemo.models.Person;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PersonMapper{
     private final ModelMapper modelMapper;
 
-    @Autowired
     public PersonMapper() {
         this.modelMapper = new ModelMapper();
     }
